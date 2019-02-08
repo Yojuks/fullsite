dbfe6196914431f1ef9a17b2560c006d2d47a1dc
Andrii Yozh <Andriiyozh@Volodymyrs-MacBook-Pro-3.local>
2019-02-08 19:29:07 +0200

    Added working script for link next

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,33 @@ var nextLink = document.getElementsByClassName('next__link')[0];
 var parent = document.getElementById('parent');
 var kid = document.getElementsByClassName('slider-item');
 
-function slider() {
-   for (var j=0; j< kid.length; j++) {
-        kid[j].classList.add('slider-item');
-        kid[0].classList.add('slider-item__active');
 
-        // я хочу щоб якщо є вже один клас то обнулити всі елементи(їх класи) і вивести вже 2ий елемент
-        if (kid[0].classList.contains('slider-item__active')) { 
-            nextLink.removeEventListener('click', slider);  // а поки виводиться 2 відразу
-            nextLink.addEventListener('click', slider);     
-            kid[j].classList.add('slider-item');
-            kid[1].classList.add('slider-item__active');
-        }
-    }
+
+var flag = 0;
+function slider() {
+    if (flag == 0) {
+        kid[0].classList.add('slider-item__active');
+        flag = 1;
+   } else if (flag == 1) {
+        kid[0].classList.remove('slider-item__active');
+        kid[1].classList.add('slider-item__active');
+        flag = 2;
+   } else if (flag == 2) {
+        kid[2].classList.add('slider-item__active');
+        kid[1].classList.remove('slider-item__active');
+        flag = 3;
+    } else if (flag == 3) {
+        kid[3].classList.add('slider-item__active');
+        kid[2].classList.remove('slider-item__active');
+        flag = 4;
+    } else if (flag == 4) {
+        kid[4].classList.add('slider-item__active');
+        kid[3].classList.remove('slider-item__active');
+        flag = 5;
+} 
 }
 
-nextLink.addEventListener('click', slider);
+nextLink.addEventListener('click', slider, false);
 
 
 function slider1() {
