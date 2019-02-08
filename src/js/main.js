dc7e512cedcc06985531754cde02a335c16dd2e7
Andrii Yozh <Andriiyozh@Volodymyrs-MacBook-Pro-3.local>
2019-02-08 14:31:13 +0200

    Added new script for slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,18 @@
 var backLink = document.getElementsByClassName('back__link')[0];
-console.log(backLink);
-
 var nextLink = document.getElementsByClassName('next__link')[0];
-console.log(nextLink);
 
-var parent = document.getElementById('parent');
-console.log(parent.children.length-2);
+
 
 function slider() {
-    for (var i = 0; i < parent.children.length-2; i++) {
-        console.log(parent.children.length-2);
-        
-        parent.children.length[i].classList.add('slider-item__active');
-
-        console.log(parent.children.length[i]);
+    var parent = document.getElementById('parent');
+    console.log(parent);
+    var kid = document.getElementsByClassName('slider-item');
+    console.log(kid);
+    for (var j=0; j< kid.length; j++) {
+        kid[j].classList.add('slider-item__active');
+        kid[length + j].classList.add('slider-item__hidden');
+        nextLink.removeEventListener('click', slider);
+        nextLink.addEventListener('click', slider);
     }
 }
 
@@ -21,6 +20,21 @@ nextLink.addEventListener('click', slider);
 
 
 
-    
+
+// var i = 0;
+// var ru = ['идти','бежать', 'спать'];
+// var p = document.getElementById('ru')
+// function f1() {
+//       p.innerHTML = ru[i++];
+//       i = i % 3
+//   }
+// <p id="ru"> </p>
+// <button onclick="f1()">Push</button>
+
+
+
+
+
+
 
 
