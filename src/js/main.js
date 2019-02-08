e684ee3fd22f490d06cb47ebf7688129909d7e59
Andrii Yozh <Andriiyozh@Volodymyrs-MacBook-Pro-3.local>
2019-02-08 14:58:44 +0200

    Added script for slider 2button ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,29 @@
 var backLink = document.getElementsByClassName('back__link')[0];
 var nextLink = document.getElementsByClassName('next__link')[0];
 
+var parent = document.getElementById('parent');
+var kid = document.getElementsByClassName('slider-item');
+
 
 
 function slider() {
-    var parent = document.getElementById('parent');
-    console.log(parent);
-    var kid = document.getElementsByClassName('slider-item');
-    console.log(kid);
-    for (var j=0; j< kid.length; j++) {
+   for (var j=0; j< kid.length; j++) {
         kid[j].classList.add('slider-item__active');
         kid[length + j].classList.add('slider-item__hidden');
-        nextLink.removeEventListener('click', slider);
-        nextLink.addEventListener('click', slider);
     }
 }
 
 nextLink.addEventListener('click', slider);
 
 
+function slider1() {
+    for (var i=0; i< kid.length; i++) {
+        kid[i].classList.remove('slider-item__active');
+        kid[i].classList.add('slider-item__hidden');
+     }
+ }
 
-
-// var i = 0;
-// var ru = ['идти','бежать', 'спать'];
-// var p = document.getElementById('ru')
-// function f1() {
-//       p.innerHTML = ru[i++];
-//       i = i % 3
-//   }
-// <p id="ru"> </p>
-// <button onclick="f1()">Push</button>
-
-
+ backLink.addEventListener('click', slider1);
 
 
 
