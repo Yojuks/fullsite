344515b15d2b2ed7d37cebe27d06fd0d6be80cb2
Andrii Yozh <Andriiyozh@Volodymyrs-MacBook-Pro-3.local>
2019-02-13 19:03:46 +0200

    Added mistakes

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,169 @@
 var backLink = document.getElementsByClassName('back__link')[0];
 var nextLink = document.getElementsByClassName('next__link')[0];
-
-var parent = document.getElementById('parent');
+var parent = document.getElementById('slider');
 var kid = document.getElementsByClassName('slider-item');
 
 
+// $(function() {
+    function goPrev() {
+        var kids = document.getElementsByClassName('slider-item');
+        console.log(kids);
+        
+        function elementIsActive() {
+            for (var i = 0; i < kids.length; i++) {
+                if (kids[i].classList.contains('slider-item__active')) {
+                     return kids[i];
+                } 
+            }
+        }
+        var activeElement = elementIsActive();
+        if (activeElement.previousSibling.previousSibling == null) {
+            return;
+         }
 
-var flag = 0;
-function slider() {
-    if (flag == 0) {
-        kid[0].classList.add('slider-item__active');
-        flag = 1;
-   } else if (flag == 1) {
-        kid[0].classList.remove('slider-item__active');
-        kid[1].classList.add('slider-item__active');
-        flag = 2;
-   } else if (flag == 2) {
-        kid[2].classList.add('slider-item__active');
-        kid[1].classList.remove('slider-item__active');
-        flag = 3;
-    } else if (flag == 3) {
-        kid[3].classList.add('slider-item__active');
-        kid[2].classList.remove('slider-item__active');
-        flag = 4;
-    } else if (flag == 4) {
-        kid[4].classList.add('slider-item__active');
-        kid[3].classList.remove('slider-item__active');
-        flag = 5;
-} 
-}
+        if (activeElement.previousSibling) {
+            activeElement.classList.remove('slider-item__active');
+            
+            activeElement.previousSibling.previousSibling.classList.add('slider-item__active');
+            
+            
+             // якщо нема елемента як зробити щоб в консолі помилок не було?
+             
+        }
+    }
+    
+    function goNext() {
+        // отримати всю колекцію елементів 'slider-item'
+        var kids = document.getElementsByClassName('slider-item');
+        
+        //взнати елемент який зараз активний
+        function elementIsActive() {
+            for (var i = 0; i < kids.length; i++) {
+                if (kids[i].classList.contains('slider-item__active')) {
+                    return kids[i];
+                } 
+            }
+        }
+       
+        var activeElement = elementIsActive();
+        if (activeElement.nextSibling.nextSibling == null) {
+            return;
+         }
+        // дати наступному елементу активний клас а всім іншим прибрати його
+        if (activeElement.nextSibling) {
+            activeElement.nextSibling.nextSibling.classList.add('slider-item__active');
+            activeElement.classList.remove('slider-item__active');
+        }
+        
+        // задача вирішена
+    }
 
-nextLink.addEventListener('click', slider, false);
+    backLink.addEventListener('click', goPrev);
+    nextLink.addEventListener('click', goNext);
+    
+    // var myFunc = function(goNext) {goNext()}
+
+    var allDot = document.querySelectorAll('ul.slider__item > li');
+    
+    
+   
+    for (var d=0; d<allDot.length; d++) {
+        allDot[d].addEventListener('click', checkDot(goNext, goPrev));
+    }
+
+    // function name (event) {
+    //     console.log(event);
+        
+    // }
+
+    function checkDot(goNext, goPrev) {
+
+        var a = goNext();
+        // function First() {
+        //     if (allDot[d].classList.contains('slider__list-active')) {
+        //         return allDot[d];
+        //     }
+        // }
+
+        // var Name = First();
+
+        
+        
+        
+        
+            // var kids = document.getElementsByClassName('slider-item');
+            
+            // function ActiveDot() {
+            //     for(var j=0; j<allDot.length; j++) {
+            //         if (allDot[j].classList.contains('slider__list-active')) {
+            //             return allDot[j];
+            //         }
+            //     }
+            // }
+
+            // var currentActiveDot = ActiveDot();
+
+            
+
+    //!!!!!Uncaught RangeError: Maximum call stack size exceeded
+            // function Click() {
+            //     for (var d=0; d<allDot.length; d++) {
+            //         allDot[d].addEventListener('click', checkDot(goNext, goPrev));
+            //     }
+            // }
+            
+            // var currentClick = Click();
+
+            // if (currentClick) {
+            //     currentActiveDot.classList.remove('slider__list-active');
+            // }                                                          
 
 
-function slider1() {
-    for (var i=0; i< kid.length; i++) {
-        kid[i].classList.remove('slider-item__active');
-     }
- }
+        }
+       
+            
+        
 
- backLink.addEventListener('click', slider1);
+        
+        
+    
+    //     
+    //      var activeElement = elementIsActive();
+     
+    
+    // });
+    
 
+// for (var key in kid) {
+//     console.log(key); // 0, 1, 2, length, item - неможна перебирати - а що використати? перетворити в масив?
+  
 
-
-
-
-
+    // var slideIndex = 1;
+    // showSlides(slideIndex);
+    
+    // // Next/previous controls
+    // function plusSlides(n) {
+    //   showSlides(slideIndex += n);
+    // }
+    
+    // // Thumbnail image controls
+    // function currentSlide(n) {
+    //   showSlides(slideIndex = n);
+    // }
+    
+    // function showSlides(n) {
+    //   var i;
+    //   var slides = document.getElementsByClassName("slider-item");
+    //   var dots = document.getElementsByClassName("slider__list");
+    //   if (n > slides.length) {slideIndex = 1} 
+    //   if (n < 1) {slideIndex = slides.length}
+    //   for (i = 0; i < slides.length; i++) {
+    //       slides[i].style.display = "none"; 
+    //   }
+    //   for (i = 0; i < dots.length; i++) {
+    //       dots[i].className = dots[i].className.replace(" active", "");
+    //   }
+    //   slides[slideIndex-1].style.display = "block"; 
+    //   dots[slideIndex-1].className += " active";
+    // }
+    
