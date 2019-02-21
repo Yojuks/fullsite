b88aa3e072d4c5e334be6fed5074d83cf7e5943d
Andrii Yozh <Andriiyozh@Volodymyrs-MacBook-Pro-3.local>
2019-02-21 16:11:46 +0200

    Added working script with next and previous button

--- src/js/main.js
+++ src/js/main.js
@@ -19,22 +19,35 @@ var kid = document.getElementsByClassName('slider-item');
         if (activeElement.previousSibling.previousSibling == null) {
             return;
          }
-
         if (activeElement.previousSibling) {
             activeElement.classList.remove('slider-item__active');
-            
             activeElement.previousSibling.previousSibling.classList.add('slider-item__active');
-            
-            
-             // якщо нема елемента як зробити щоб в консолі помилок не було?
-            //  highlight();
+
+        }
+        var dots = document.getElementsByClassName('slider__list');
+      
+        function dotsIsActive() {
+            for (var i = 0; i < dots.length; i++) {
+                if (dots[i].classList.contains('slider__list-active')) {
+                    return dots[i];
+                } 
+            }
+        }
+
+        var activeDot = dotsIsActive();
+
+        if (activeDot.previousSibling.previousSibling == null) {
+            return;
+        }
+        if (activeDot.previousSibling) {
+            activeDot.previousSibling.previousSibling.classList.add('slider__list-active');
+            activeDot.classList.remove('slider__list-active');
         }
     }
-    
+
     function goNext() {
         // отримати всю колекцію елементів 'slider-item'
         var kids = document.getElementsByClassName('slider-item');
-        
         //взнати елемент який зараз активний
         function elementIsActive() {
             for (var i = 0; i < kids.length; i++) {
@@ -44,6 +57,7 @@ var kid = document.getElementsByClassName('slider-item');
             }
         }
         var activeElement = elementIsActive();
+        
         if (activeElement.nextSibling.nextSibling == null) {
             return;
          }
@@ -52,40 +66,52 @@ var kid = document.getElementsByClassName('slider-item');
             activeElement.nextSibling.nextSibling.classList.add('slider-item__active');
             activeElement.classList.remove('slider-item__active');
         }
+
         // highlight();
         // задача вирішена
+        var dots = document.getElementsByClassName('slider__list');
+      
+        function dotsIsActive() {
+            for (var i = 0; i < dots.length; i++) {
+                if (dots[i].classList.contains('slider__list-active')) {
+                    return dots[i];
+                } 
+            }
+        }
+
+        var activeDot = dotsIsActive(); 
+
+        if (activeDot.nextSibling.nextSibling == null) {
+            return;
+        }
+        if (activeDot.nextSibling) {
+            activeDot.nextSibling.nextSibling.classList.add('slider__list-active');
+            activeDot.classList.remove('slider__list-active');
+        }
     }
 
     backLink.addEventListener('click', goPrev);
     nextLink.addEventListener('click', goNext);
-    
     var selectedLi;
     var selectedSlide;
-
     var allDot = document.getElementsByTagName('ul')[1];
     var index = 1;
+
     var callback = function(index) {
-
       return function(event) {
-
         // if (target.tagName != 'li') return;
         var target = event.target;
-        
-
         // if (activeElement.classList.contains('slider-item__active')) {
         //     target.event.classList.remove('slider-item__active');
         // }
-
         for (var j=0; j<event.target.length; j++) {
             if(event.target) {
                 event.target[i] = kids[i];
+                console.log(kids[i]);
+                
             } 
         }
-
-        // event.target = goNext()
-
         highlight(target);
-        // currentSlide(target);
     }
 }
 
@@ -101,123 +127,31 @@ var kid = document.getElementsByClassName('slider-item');
         if (selectedLi) {
             selectedLi.classList.remove('slider__list-active');
         }
-        
         selectedLi = index;
         selectedLi.classList.add('slider__list-active');
 
-        // var kids = document.getElementsByClassName('slider-item');
-    
-        // function elementIsActive() {
-        //     for (var i = 0; i < kids.length; i++) {
-        //         if (kids[i].classList.contains('slider-item__active')) {
-        //             return kids[i];
-        //         } 
-        //     }
-        // }
 
-        // var activeElement = elementIsActive();
-        // activeElement.classList.remove('slider-item__active');
-        // activeElement.classList.add('slider-item__active');
-
-
-        // var slideIndex = 1;
-        // showSlide(slideIndex);
-    
-        // function plusSlides(n){
-        // showSlide(slideIndex += n);
-        // }
-    
-        // function currentSlide(n) {
-        // showSlide(slideIndex = n);
-        // }
+        function activeSlider(index) {
+                for (var i = 0; i < kids.length; i++) {
+                    if (kids[i].classList.contains('slider-item__active')) {
+                        return kids[i];
+                    } 
+                }
+            }
+            var activeSlider = activeSlider();
         
-        // function showSlide(n){
-        // var i;
-        // var slides = document.getElementsByClassName('slider-item');
-        // // var slides = document.getElementsByClassName("myslides");
-        // // var dots = document.getElementsByClassName("dots");
-        // var dots = document.getElementsByTagName('ul')[1];
+            var dots = document.getElementsByClassName('slider__list');
 
-        // if (n > slides.length)
-        //  { slideIndex = 1};
-        // if (n < 1) {
-        //      slideIndex = slides.length
-        // };
-        // for (i=0;i<slides.length;i++) {
-        //      slides[i].style.display = "none";
-        // };
-        // for (i=0;i<dots.length;i++) {
-        //     dots[i].className = dots[i].className.replace(" slider__list-active","");
-        // };
-        //     slides[slideIndex-1].style.display = "block";
-        //     dots[slideIndex-1].className += " slider__list-active";
-        // }
+            for (var i = 0; i < kids.length; i++) {
+                index =  kids[i].dataset.pos;
+            }
+
         }
 
         
-        
-        
-        
-        
-
-
-
-let corousels = document.getElementsByClassName('slider');
-
-[].forEach.call(corousels, function (c) {
-    let next = c.getElementsByClassName('next__link')[0],
-        prev = c.getElementsByClassName('back__link')[0],
-        bubblesContainer = c.getElementsByClassName('slider__item')[0],
-        inner = c.getElementsByClassName('slider__text')[0],
-        imgs = c.getElementsByClassName('slider-item'),
-        currentImageIndex = 0,
-        width = 1280,
-        bubbles = [];
-        
-        
        
         
-        for (let i = 0; i < imgs.length; i++) {
-            let b = document.createElement('span');
-            b.classList.add('bubble');
-            bubblesContainer.appendChild(b);
-            bubbles.push(b);
-            console.log(bubbles);
-            
+        
 
-            b.addEventListener('click', function () {
-                currentImageIndex = i;
-                switchImg();
-            });
-        };
 
-        function switchImg() {
-            inner.style.left = -width * currentImageIndex + "px";
 
-            bubbles.forEach(function (b, i) {
-                if (i === currentImageIndex) {
-                    b.classList.add('slider-item__active');
-                } else {
-                    b.classList.remove('slider-item__active')
-                }
-            })
-        };
-
-        next.addEventListener('click', function () {
-            currentImageIndex++;
-
-            if (currentImageIndex >= imgs.length) {
-                currentImageIndex = 0;
-            }
-            switchImg();
-        });
-
-        prev.addEventListener('click', function () {
-            currentImageIndex--;
-
-            if (currentImageIndex < 0) {
-                currentImageIndex = imgs.length - 1;
-            }
-            switchImg();
-        });
-})
