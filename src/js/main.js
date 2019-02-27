574f44411d5f6d8cc640869e323a2371c068dbbb
Andrii Yozh <Andriiyozh@Volodymyrs-MacBook-Pro-3.local>
2019-02-27 22:29:39 +0200

    Fixed short menu and small bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,10 @@
-
-
 var backLink = document.getElementsByClassName('back__link')[0];
 var nextLink = document.getElementsByClassName('next__link')[0];
 var kid = document.getElementsByClassName('slider-item');
 
-
 // $(function() {
     function goPrev() {
         var kids = document.getElementsByClassName('slider-item');
-        
         function elementIsActive() {
             for (var i = 0; i < kids.length; i++) {
                 if (kids[i].classList.contains('slider-item__active')) {
@@ -26,7 +22,6 @@ var kid = document.getElementsByClassName('slider-item');
 
         }
         var dots = document.getElementsByClassName('slider__list');
-      
         function dotsIsActive() {
             for (var i = 0; i < dots.length; i++) {
                 if (dots[i].classList.contains('slider__list-active')) {
@@ -34,9 +29,7 @@ var kid = document.getElementsByClassName('slider-item');
                 } 
             }
         }
-
         var activeDot = dotsIsActive();
-
         if (activeDot.previousSibling.previousSibling == null) {
             return;
         }
@@ -45,7 +38,6 @@ var kid = document.getElementsByClassName('slider-item');
             activeDot.classList.remove('slider__list-active');
         }
     }
-
     function goNext() {
         // отримати всю колекцію елементів 'slider-item'
         var kids = document.getElementsByClassName('slider-item');
@@ -58,7 +50,6 @@ var kid = document.getElementsByClassName('slider-item');
             }
         }
         var activeElement = elementIsActive();
-        
         if (activeElement.nextSibling.nextSibling == null) {
             return;
          }
@@ -67,11 +58,8 @@ var kid = document.getElementsByClassName('slider-item');
             activeElement.nextSibling.nextSibling.classList.add('slider-item__active');
             activeElement.classList.remove('slider-item__active');
         }
-
-        // highlight();
         // задача вирішена
         var dots = document.getElementsByClassName('slider__list');
-      
         function dotsIsActive() {
             for (var i = 0; i < dots.length; i++) {
                 if (dots[i].classList.contains('slider__list-active')) {
@@ -79,9 +67,7 @@ var kid = document.getElementsByClassName('slider-item');
                 } 
             }
         }
-
         var activeDot = dotsIsActive(); 
-
         if (activeDot.nextSibling.nextSibling == null) {
             return;
         }
@@ -90,11 +76,9 @@ var kid = document.getElementsByClassName('slider-item');
             activeDot.classList.remove('slider__list-active');
         }
     }
-
     backLink.addEventListener('click', goPrev);
     nextLink.addEventListener('click', goNext);
     var selectedLi;
-    var selectedSlide;
     var allDot = document.getElementsByTagName('ul')[1];
     var index = 1;
 
@@ -102,20 +86,9 @@ var kid = document.getElementsByClassName('slider-item');
       return function(event) {
         // if (target.tagName != 'li') return;
         var target = event.target;
-        // if (activeElement.classList.contains('slider-item__active')) {
-        //     target.event.classList.remove('slider-item__active');
-        // }
-        // for (var j=0; j<event.target.length; j++) {
-        //     if(event.target) {
-        //         event.target[i] = kids[i];
-        //         console.log(kids[i]);
-                
-        //     } 
-        // }
         highlight(target);
+    }   
     }
-}
-
     allDot.addEventListener('click', callback(index));
    
     function highlight(index) {
@@ -130,8 +103,6 @@ var kid = document.getElementsByClassName('slider-item');
         }
         selectedLi = index;
         selectedLi.classList.add('slider__list-active');
-    
-        console.log(selectedLi.dataset.pos);
 
         var sliders = document.getElementsByClassName('slider-item');
 
@@ -146,10 +117,50 @@ var kid = document.getElementsByClassName('slider-item');
             
         }
 
+        // Slider on header1
+        // for short menu - block is hidden
+       $('.menu-short').on('click', function(e) {
+           e.preventDefault();
+          $('.container-menu').toggleClass('activeMenu');
+          $('.header-main__wrapper--float').toggleClass('activeContent');
+       });
         
+    //    for header1
+        var selectedLi2;
+        var allDot2 = document.getElementsByTagName('ul')[3];
+        var index1 = 1;
+
+        var callback1 = function(index1) {
+            return function(event) {
+              // if (target.tagName != 'li') return;
+              var target = event.target;
+              highlight1(target);
+          }
+      }
+
+      allDot2.addEventListener('click', callback1(index1));
+   
+    function highlight1(index1) {
+        
+
+        var allDot3 = document.getElementsByTagName('ul')[3].children[0];
        
-        
-        
+         allDot3.classList.remove('slider__list-active');
+    
+        if (selectedLi2) {
+            selectedLi2.classList.remove('slider__list-active');
+        }
+        selectedLi2 = index1;
+        selectedLi2.classList.add('slider__list-active');
 
+        var sliders1 = document.getElementsByClassName('slider-item1');
 
-
+            for (var i = 0; i < sliders1.length; i++) {
+                if (selectedLi2.dataset.pos === sliders1[i].dataset.pos) {
+                    sliders1[i].classList.add('slider-item__active');
+                } else {
+                    sliders1[i].classList.remove('slider-item__active');
+                }
+            }
+            
+        }
